e8ecc3dff4036d8b96b4eac85680453940a21e74
Matthew O <mo@pop-os.localdomain>
2020-11-05 19:31:33 -0700

    setup server file

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+// dotenv to hide session secret
+require("dotenv").config();
+
+// Will use for later
+// const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SESS_SCRT,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Commenting out for later use
+// const hbs = exphbs.create({});
+
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(require("./controllers/"));
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
