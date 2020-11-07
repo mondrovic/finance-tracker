cddcf3a100e660dae2f95e9d98e52a00089301d7
Matthew O <mo@pop-os.localdomain>
2020-11-06 21:06:35 -0700

    added index associates

--- models/index.js
+++ models/index.js
@@ -2,15 +2,23 @@ const Bill = require('./Bill');
 const Category = require('./Category');
 const User = require('./User');
 
-Bill.belongsTo(Category);
+Bill.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: 'CASCADE'
+});
 
-Category.hasMany(Bill);
+Category.hasMany(Bill, {
+  foreignKey: "category_id"
+});
 
 Bill.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
-  });
+});
 
+User.hasMany(Bill, {
+  foreignKey: "user_id"
+})
 
 
 module.exports = {
