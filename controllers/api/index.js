2c54bcda52e6353d0e9955432eda1517f9b920f2
Matthew O <mo@pop-os.localdomain>
2020-11-07 20:22:28 -0700

    added basic category route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const categoryRoutes = require("./category-routes");
 
 router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
