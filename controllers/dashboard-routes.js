a241876f2b6b71daa94f00b16c526940083a5bfa
ry brown <ryan.alex.brown@gmail.com>
2020-11-08 16:25:14 -0700

    dashboard

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { Bill } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", (req, res) => {
+    Bill.findAll({
+      where: {
+      }
+    })
+      .then(dbBillData => {
+        const bills = dbBillData.map((bill) => bill.get({ plain: true }));
+        
+        res.render("all-bills-admin", {
+          layout: "dashboard",
+          bills
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        
+      });
+  });
+
+  router.get("/new", (req, res) => {
+    res.render("new-bill", {
+      layout: "dashboard"
+    });
+  });
+  
+  router.get("/edit/:id", (req, res) => {
+    Bill.findByPk(req.params.id)
+      .then(dbBillData => {
+        if (dbBillData) {
+          const bill = dbBillData.get({ plain: true });
+          
+          res.render("edit-bill", {
+            layout: "dashboard",
+            bill
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const homeRoutes = require("./home-routes");
 const apiRoutes = require("./api");
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
+
 
 module.exports = router;
