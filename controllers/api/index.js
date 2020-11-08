bb29c44fa8c3657ff7dbb33b9b73e9f378a06c83
Matthew O <mo@pop-os.localdomain>
2020-11-07 20:39:49 -0700

    added base api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const categoryRoutes = require("./category-routes");
+const billRoutes = require("./bill-routes");
 
 router.use("/users", userRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/bills", billRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,28 +1,27 @@
-const Bill = require('./Bill');
-const Category = require('./Category');
-const User = require('./User');
+const Bill = require("./Bill");
+const Category = require("./Category");
+const User = require("./User");
 
 Bill.belongsTo(Category, {
   foreignKey: "category_id",
-  onDelete: 'CASCADE'
+  onDelete: "CASCADE",
 });
 
 Category.hasMany(Bill, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
 });
 
-// Bill.belongsTo(User, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// });
+Bill.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-// User.hasMany(Bill, {
-//   foreignKey: "user_id"
-// })
-
+User.hasMany(Bill, {
+  foreignKey: "user_id",
+});
 
 module.exports = {
-    Bill,
-    Category,
-    User
-}
+  Bill,
+  Category,
+  User,
+};
