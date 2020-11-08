b3912e2aead9a37234fb99fce1c0fd2eeb73a10a
luc1dLife <luc1d_life@protonmail.com>
2020-11-07 23:53:33 -0700

    Initial Additions

---
+++ models/home-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { Bill, Category, User } = require("../models");
+
+// Render all Posts
+router.get("/", (req, res) => {
+  console.log(req.session);
+
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Redirect User to Homepage upon Authentication
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+// Render Sign Up Page
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+
+      // Serialize Data
+      const post = dbPostData.get({ plain: true });
+
+      // Template the Data
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
