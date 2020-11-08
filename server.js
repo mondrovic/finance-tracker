a622bc8481f4dea0a7da71ccc3404561efb47eac
luc1dLife <luc1d_life@protonmail.com>
2020-11-08 03:21:19 -0700

    + Argument Handler

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const arg = process.argv[2];
+const version = "1.0.0";
 const session = require("express-session");
 // dotenv to hide session secret
 require("dotenv").config();
@@ -23,20 +25,51 @@ const sess = {
   }),
 };
 
-app.use(session(sess));
+// Initialize Application
+init = () => {
+  app.use(session(sess));
 
-// Commenting out for later use
-// const hbs = exphbs.create({});
+  // Commenting out for later use
+  // const hbs = exphbs.create({});
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+  // app.engine("handlebars", hbs.engine);
+  // app.set("view engine", "handlebars");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers"));
+  app.use(require("./controllers"));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+  });
+};
+
+// Display Argument Data
+if (arg === "-h") {
+  console.log(`
+        Usage: node server.js [ -h | -v | -l | -a ]
+        or: npm start
+        
+        [options]
+        -h          Display this message.
+        -v          Show version.
+        -l          Show license info.
+        -a          What is Finance Tracker?
+    `);
+  process.exit();
+} else if (arg === "-v") {
+  console.log("Finance Tracker Version: " + version);
+  process.exit();
+} else if (arg === "-l") {
+  console.log("Licensed under the Creative Commons CC0 Public Domain.");
+  process.exit();
+} else if (arg === "-a") {
+  console.log(
+    "Full stack Financial application utilizing ORM with Sequelize and adhering to the Model-View-Controller paradigm."
+  );
+  process.exit();
+} else {
+  init();
+}
