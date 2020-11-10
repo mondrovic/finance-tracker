7f2d1347c40624f2ed4b3e169b9119c87278aba8
luc1dLife <luc1d_life@protonmail.com>
2020-11-09 18:22:04 -0700

    /dashboard/new view

--- public/javascript/add-bill.js
+++ public/javascript/add-bill.js
@@ -1,32 +1,30 @@
-const newFormHandler = async function(event) {
-    event.preventDefault();
+const newFormHandler = async function (event) {
+  event.preventDefault();
 
-    const bill_name = document.querySelector('input[name="bill-type"]').value;
-    const amount = document.querySelector('input[name="bill-amount"]').value;
-    const category_id = document.querySelector('input[name="category_id"]').value;
-    console.log(bill_name + " / " + amount + " / " + category_id );
+  const bill_name = document.querySelector('input[name="bill-name"]').value;
+  const amount = document.querySelector('input[name="bill-amount"]').value;
+  const category_id = document.querySelector('input[name="category_id"]').value;
+  console.log(bill_name + " / " + amount + " / " + category_id);
 
-    if (bill_name && amount) { 
-        const response = await fetch(`/api/bills`, {
-            method: "POST",
-            body: JSON.stringify({
-                bill_name,
-                amount,
-                category_id
-            }),
-            headers: { "Content-Type": "application/json" },
-          });
-          if (response.ok) {
-            console.log("success");
-            document.location.replace("/dashboard");
-          } else {
-            alert(response.statusText);
-          }
-
-    };
+  if (bill_name && amount) {
+    const response = await fetch(`/api/bills`, {
+      method: "POST",
+      body: JSON.stringify({
+        bill_name,
+        amount,
+        category_id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      console.log("success");
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 
-  
-
-
-  document.querySelector("#new-bill-form").addEventListener("submit", newFormHandler);
+document
+  .querySelector("#new-bill-form")
+  .addEventListener("submit", newFormHandler);
